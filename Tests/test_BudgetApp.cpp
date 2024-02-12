8953510065fa1875e15764804e41d25214754be4
matkad92 <mateuszkadziolka92@gmail.com>
2024-02-12 18:08:38 +0100

    Adding GTest to project

---
+++ Tests/test_BudgetApp.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+TEST(MyBudgetTest, ExampleTestCase) {
+
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
+
+TEST(MyBudgetTest, ExampleTestCase1) {
+
+    EXPECT_EQ(1, 1);
+    EXPECT_FALSE(false);
+}
